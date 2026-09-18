ac44a328380aece23bfefb74f8b952292f46fda8
Mohammed Jamal Dawoud <MohammedJamalDawoud@users.noreply.github.com>
2026-09-18 15:35:16 +0200

    chore(package): remove redundant modules and dependencies

--- src/mri_segmentation/data/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Data discovery and inventory interfaces."""
-
-from .discovery import discover_nifti
-from .inventory import FileInventory, inventory_nifti
-
-__all__ = ["FileInventory", "discover_nifti", "inventory_nifti"]
--- src/mri_segmentation/dataset/__init__.py
+++
@@ -1,5 +0,0 @@
-"""External nnU-Net dataset-interface validation."""
-
-from .interface import DatasetCase, validate_dataset_cases
-
-__all__ = ["DatasetCase", "validate_dataset_cases"]
